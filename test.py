65fd9cb7948e691b11049c192a0fa07c9f8680c0
Norbert Widmann <nwidmann@Norberts-Air.lan>
2019-08-26 16:06:02 +0200

    Seperated into file with function and file with tests.

---
+++ test.py
@@ -0,0 +1,23 @@
+import functions.py
+
+(count_num, prime_fac) = table_stats(200)
+
+# make a list of items in the dictionary sorted by occurence
+list_of_items = sorted(count_num.items(), key=lambda x : x[1])
+
+# print the numbers, their occurrence (including 0 occurrences) and their prime factors
+for tuple in list_of_items:        
+    print(tuple[0], " : ", tuple[1], end='')
+    print(" : ", prime_fac[tuple[0]])
+
+count_hist = { }
+
+# calculate a histogram: How many numbers did occur a certain amount of times?
+for tuple in list_of_items:
+    if tuple[1] in count_hist.keys():
+        count_hist[tuple[1]] += 1
+    else:
+        count_hist[tuple[1]] = 1
+        
+# print the histogram
+print(count_hist)
